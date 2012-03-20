d5dca991b577b93ea77026a8195092f130f8ee56
CosmonauT <cosmonaut@mail.ua>
2012-03-20 10:02:49 -0600

    TODO changed

--- dskb.lisp
+++ dskb.lisp
@@ -1,4 +1,10 @@
+(defvar *dskb-keymaps* '())
 
+(defvar *dskb-switch* "")
+
+(defvar *dskb-led* "")
+
+(defvar *setxkbmap-command* "/usr/bin/setxkbmap")
 
 
 (defstruct dskb-keymap
@@ -28,8 +34,19 @@
 
 
 
+(defmacro add-dskb-map (name layout-name variants)
+  `(add-to-list *dskb-keymaps* (make-dskb-keymap
+				:name ,name
+				:layout ,layout-name
+				:variants ,variants)))
 
+(defmacro add-dskb-switch (name command-string)
+  `(add-to-list *dskb-switch* (make-dskb-switch
+			       :name ,name
+			       :command-string ,command-string)))
 
+(defmacro add-dskb-led (name command-string)
+  `(add-to-list *dskb-led*
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Keymap definitions ;;;;
@@ -44,3 +61,7 @@
 		      (make-dskb-switch :name "Control+Alt" :command-string "ctrol_alt_toggle")))
 
 (setf dskb-leds '((make-dskb-led :name "Scroll lock" :command-string "scroll")))
+
+
+(defcommand set-xkb-map () ()
+  (
